1ce2948ba34379625f18dd92b4fa6a2e83d7f0c1
Richard Creeger <richard.creeger@gmail.com>
2018-11-05 14:25:06 -0700

    reducer practice complete

--- redux-2-reducers-practice/src/practice/reducers/reducers-practice-2.js
+++ redux-2-reducers-practice/src/practice/reducers/reducers-practice-2.js
@@ -1,6 +1,6 @@
-/* 
+/*
   Problem #2
-  
+
   Export a reducer that will manages an array of groceries. It must expect the following action types:
     - ADD_GROCERY
     - FETCH_GROCERIES
@@ -8,8 +8,29 @@
 */
 
 // Theoretically, these action type labels would be imported from an actions file, but we will define them here in this case.
-let ADD_GROCERY = 'ADD_GROCERY'
-let FETCH_GROCERIES = 'FETCH_GROCERIES'
-let REMOVE_GROCERY = 'REMOVE_GROCERY'
+let ADD_GROCERY = "ADD_GROCERY";
+let FETCH_GROCERIES = "FETCH_GROCERIES";
+let REMOVE_GROCERY = "REMOVE_GROCERY";
 
 // Write your code below this line!
+let initialState = 0;
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_GROCERY:
+      let newGrocery = {
+        id: 4,
+        item: action.payload,
+        quantity: 3
+      };
+      return [...state, newGrocery];
+    case FETCH_GROCERIES:
+      return action.payload;
+    case REMOVE_GROCERY:
+      let theId = action.payload;
+      return state.filter(item => item.id !== Number(theId));
+
+    default:
+      return state;
+  }
+};
--- redux-2-reducers-practice/src/practice/reducers/reducers-practice-3.js
+++ redux-2-reducers-practice/src/practice/reducers/reducers-practice-3.js
@@ -1,6 +1,6 @@
-/* 
+/*
   Problem #2
-  
+
   Export a reducer that will manages an array of groceries. It must expect the following action types:
     - ADD_CONTACT
     - FETCH_CONTACTS
@@ -8,8 +8,30 @@
 */
 
 // Theoretically, these action type labels would be imported from an actions file, but we will define them here in this case.
-let ADD_CONTACT = 'ADD_CONTACT'
-let FETCH_CONTACTS = 'FETCH_CONTACTS'
-let REMOVE_CONTACT = 'REMOVE_CONTACT'
+let ADD_CONTACT = "ADD_CONTACT";
+let FETCH_CONTACTS = "FETCH_CONTACTS";
+let REMOVE_CONTACT = "REMOVE_CONTACT";
 
 // Write your code below this line!
+
+let initialState = 0;
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_CONTACT:
+      let newContact = {
+        id: 4,
+        fname: action.payload,
+        quantity: 4
+      };
+      return [...state, newContact];
+    case FETCH_CONTACTS:
+      return action.payload;
+    case REMOVE_CONTACT:
+      let theId = action.payload;
+      return state.filter(fname => fname.id !== Number(theId));
+
+    default:
+      return state;
+  }
+};
