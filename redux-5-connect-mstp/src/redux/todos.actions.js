9e98ff31e2bc9f5d95fb04d124f8c933a7165b20
Richard Creeger <richard.creeger@gmail.com>
2018-11-07 11:02:23 -0700

    it works

--- redux-5-connect-mstp/src/redux/todos.actions.js
+++ redux-5-connect-mstp/src/redux/todos.actions.js
@@ -1,16 +1,20 @@
-export const ADD_TODO = 'ADD_TODO'
-export const REMOVE_TODO = 'REMOVE_TODO'
+export const ADD_TODO = "ADD_TODO";
+export const REMOVE_TODO = "REMOVE_TODO";
 
-export const addTodo = (todo) => {
+export const addTodo = todo => {
   return {
     type: ADD_TODO,
-    payload: todo
-  }
-}
+    payload: {
+      id: Math.random(),
+      title: todo,
+      completed: false
+    }
+  };
+};
 
-export const removeTodo = (id) => {
+export const removeTodo = id => {
   return {
     type: REMOVE_TODO,
     payload: id
-  }
-}
+  };
+};
